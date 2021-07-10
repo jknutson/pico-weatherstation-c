5e14c8e1ac1d67b87a3f175e6af0ae8b2c5db5a5
John Knutson <john.m.knutson@gmail.com>
2021-07-09 20:37:54 -0500

    add rain sensor bits

---
+++ pyconsole-readonly.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import json
+import os
+import serial
+import paho.mqtt.client as mqtt
+
+with serial.Serial('/dev/ttyACM0', 115200) as ser:
+    while(True):
+        line = ser.readline().strip().decode('ascii')
+        print(line)
--- weatherstation.c
+++ weatherstation.c
@@ -17,12 +17,15 @@
 #define LED_PIN PICO_DEFAULT_LED_PIN
 #endif
 
+#define getName(var)  #var
+
 const uint DHT_PIN = 15;
 const uint MAX_TIMINGS = 85;
 const uint CM_IN_KM = 100000.0;
 const uint SECS_IN_HOUR = 3600;
 const float ANEMOMETER_RADIUS = 9;  // cm
 const uint ANEMOMETER_PIN = 2;
+const uint RAIN_PIN = 3;
 const uint ANEMOMETER_DEBOUNCE_MS = 20;
 // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
 const float ADC_CONVERSION_FACTOR = 3.3f / (1 << 12);
@@ -30,9 +33,8 @@ const int SLEEP_INTERVAL_MS = 5000; // ms
 const float R2 = 4700.0;
 const float VIN = 3.3;
 
-#define DEBOUNCE_MS 20
-static bool is_debounceing = false;
-static int gpio_cb_cnt = 0;
+static int gpio_wind_cb_cnt = 0;
+static int gpio_rain_cb_cnt = 0;
 
 typedef struct {
 	bool crc_match;
@@ -42,27 +44,21 @@ typedef struct {
 
 void read_from_dht(dht_reading *result);
 
-int64_t debounce_alarm_callback(alarm_id_t id, void *user_data) {
-	is_debounceing = false;
-	return 0;
-}
-
-bool debounce() {
-	if (!is_debounceing) {
-		add_alarm_in_ms(DEBOUNCE_MS, &debounce_alarm_callback, NULL, false);
-		is_debounceing = true;
-		return false;
+void gpio_cb(uint gpio, uint32_t events) {
+	if (gpio == ANEMOMETER_PIN) {
+		gpio_wind_cb_cnt++;
 	}
-	return true;
+	if (gpio == RAIN_PIN) {
+		gpio_rain_cb_cnt++;
+	}
 }
 
-// TODO: support multiple gpio pins
-void gpio_cb() {
-	gpio_acknowledge_irq(2, IO_IRQ_BANK0);
-	if (debounce()) return;
-	gpio_cb_cnt++;
+// TODO: implement
+float calc_rainfall(int tips) {
+	return 0.0f;
 }
 
+// TODO: move wind stuff to wind_direction.c
 float calc_wind_speed_kmh(int rotations) {
 	// speed = ( (signals/2) * (2 * pi * radius) ) / time
 	float speed_km_s = ((rotations / 2) * (2 * 3.1415 * ANEMOMETER_RADIUS)) / CM_IN_KM;
@@ -74,26 +70,21 @@ float kmh_to_mph(float kmh) {
 	return (kmh * 0.621371);
 }
 
-void reset_speed_counter(int* count) {
-#ifdef DEBUG
-	printf("resetting counter %i -> 0\n", *count);
-#endif
+void reset_counter(int* count) {
 	*count = 0;
 }
 
 int main() {
 	stdio_init_all();
 	gpio_init(DHT_PIN);
+
 #ifdef LED_PIN
 	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
 #endif
-	gpio_set_pulls(ANEMOMETER_PIN, false, true);  // pull down
-	irq_set_exclusive_handler(IO_IRQ_BANK0, gpio_cb);
-	gpio_set_irq_enabled(ANEMOMETER_PIN, GPIO_IRQ_EDGE_RISE, true);
-	irq_set_enabled(IO_IRQ_BANK0, true);
 
-	// gpio_set_irq_enabled_with_callback(ANEMOMETER_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_cb);
+	gpio_set_pulls(ANEMOMETER_PIN, false, true);  // pull down
+	gpio_set_irq_enabled_with_callback(ANEMOMETER_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_cb);
 
 	adc_init();
 	adc_gpio_init(26);
@@ -117,14 +108,23 @@ int main() {
 			printf("{\"wind_angle\": %f}\n", get_angle(R2, VIN, result_v));
 		}
 		// calculate wind speed
-		float wind_speed_kmh = calc_wind_speed_kmh(gpio_cb_cnt);
+		float wind_speed_kmh = calc_wind_speed_kmh(gpio_wind_cb_cnt);
 		float wind_speed_mph = kmh_to_mph(wind_speed_kmh);
-		printf("{\"wind_speed\": %.2f, \"wind_speed_kmh\": %.2f, \"gpio_cb_cnt\": %i}\n", wind_speed_mph, wind_speed_kmh, gpio_cb_cnt);
-		reset_speed_counter(&gpio_cb_cnt);
+		printf("{\"wind_speed\": %.2f, \"wind_speed_kmh\": %.2f, \"gpio_wind_cb_cnt\": %i}\n", wind_speed_mph, wind_speed_kmh, gpio_wind_cb_cnt);
+#ifdef DEBUG
+		printf("resetting wind counter %i -> 0\n", gpio_wind_cb_cnt);
+#endif
+		reset_counter(&gpio_wind_cb_cnt);
+		// calculate rainfall
+		float rainfall_in = calc_rainfall(gpio_rain_cb_cnt);
+		printf("{\"rainfall_in\": %.2f, \"gpio_rain_cb_cnt\": %i}\n", rainfall_in, gpio_rain_cb_cnt);
+		printf("resetting rain counter %i -> 0\n", gpio_rain_cb_cnt);
+		reset_counter(&gpio_rain_cb_cnt);
 		sleep_ms(SLEEP_INTERVAL_MS);
 	}
 }
 
+// TODO: move to dht.c/h
 void read_from_dht(dht_reading *result) {
 	int data[5] = {0, 0, 0, 0, 0};
 	uint last = 1;
@@ -178,32 +178,4 @@ void read_from_dht(dht_reading *result) {
 		printf("CRC mismatch - crc:%i actual:%i\n", data[4], ((data[0] + data[1] + data[2] + data[3]) & 0xFF));
 #endif
 	}
-}
-
-static const char *gpio_irq_str[] = {
-        "LEVEL_LOW",  // 0x1
-        "LEVEL_HIGH", // 0x2
-        "EDGE_FALL",  // 0x4
-        "EDGE_RISE"   // 0x8
-};
-
-void gpio_event_string(char *buf, uint32_t events) {
-    for (uint i = 0; i < 4; i++) {
-        uint mask = (1 << i);
-        if (events & mask) {
-            // Copy this event string into the user string
-            const char *event_str = gpio_irq_str[i];
-            while (*event_str != '\0') {
-                *buf++ = *event_str++;
-            }
-            events &= ~mask;
-
-            // If more events add ", "
-            if (events) {
-                *buf++ = ',';
-                *buf++ = ' ';
-            }
-        }
-    }
-    *buf++ = '\0';
 }
